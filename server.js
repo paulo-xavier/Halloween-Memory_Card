0d128aca04650778749780224ef537b5d76fca21
b3lodes <matheushenriquejustincaetano@gmail.com>
2024-11-13 00:19:21 -0300

    ✨ feat: Inserting the first version of server.js

--- server.js
+++ server.js
@@ -30,114 +30,67 @@ const db = mysql.createConnection({
   database: "halloween_game",
 });
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token) return res.sendStatus(401);
-  jwt.verify(token, SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user; // Se o token for válido, armazena os dados do usuário no 'req'
-    next(); // Continua para a próxima função
-  });
-}
-
 // (2)- (READ) Capturar as informações da tabela 'game';
-app.get("/game", authenticateToken, (req, res) => {
-  const userEmail = req.user.email; // Obtém o ID do usuário do token
-  db.query("SELECT email FROM users WHERE email = ?", [userEmail], (err, result) => {
+app.get("/game", async (req, res) => {
+  const { id_game } = req.body; // Obtém o ID do usuário do token
+  db.query("SELECT * FROM game WHERE id_game = ?", [id_game], (err, result) => {
     if (err) return res.status(500).json({message: "Erro de server! Tente novamente mais tarde"});
     if (result.length === 0) {
-      return res.status(404).send('Usuário não encontrado');
+      return res.status(404).send('Game not found!');
     }
     res.json(result[0]); // Retorna os dados do usuário
   });
 });
 
 // (3)- (UPDATE) Inserir ID e novo nome para dar UPDATE renomear o nome do jogador atual;
-app.put("/game", authenticateToken, async (req, res) => {
-  const { newEmail, newPassword } = req.body; // Extrai o novo e-mail e a nova senha do corpo da requisição
-  const hashedPassword = await bcrypt.hash(newPassword, 10); // Criptografa a nova senha
+app.put("/game", async (req, res) => {
+  const { id_game, player_name } = req.body; // Extrai o novo player_name e id do game atual do corpo da requisição
   db.query(
-    "UPDATE users SET email = ?, password = ? WHERE email = ?",
-    [newEmail, hashedPassword, req.user.email],
+    "UPDATE game SET player_name = ? WHERE id_game = ?",
+    [player_name, id_game],
     (err, result) => {
       if (err) throw err;
 
-      // Verifica se nenhuma linha foi afetada pela consulta (ou seja, usuário não encontrado)
+      // Verifica se nenhuma linha foi afetada pela consulta (ou seja, game não encontrado)
       if (result.affectedRows === 0) {
-        return res.status(404).send("Usuário não encontrado");
+        return res.status(404).send("Player not found!");
       }
-      res.send("Usuário atualizado com sucesso");
+      res.send("Updating name!");
     }
   );
 });
 
 // (4)- (DELETE) Deletar uma tabela de dados tipo 'game' a partir do ID;
-app.delete("/game", authenticateToken, (req, res) => {
+app.delete("/game", (req, res) => {
+  const { id_game } = req.body;
   db.query(
-    "DELETE FROM users WHERE email = ?",
-    [req.user.email],
+    "DELETE FROM game WHERE id_game = ?",
+    [id_game],
     (err, result) => {
       if (err) throw err;
 
       if (result.affectedRows === 0) {
-        return res.status(404).send("Usuário não encontrado");
+        return res.status(404).send("Game not found!");
       }
-      res.send("Usuário deletado com sucesso");
+      res.send("Game deleted successively");
     }
   );
 });
 
 // (1)- (CREATE)- Inserir dados no banco de dados da tabela 'game';
 app.post("/game", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email e senha são precisos." });
+  const { date_game, player_name, time_match, url_image, score_game } = req.body;
+  if (!date_game || !player_name || time_match || url_image || score_game ) {
+    return res.status(400).json({ message: "Error! Insert attempt with non-integer data detected!" });
   }
   db.query(
-    "SELECT * FROM users WHERE email = ?",
-    [email],
-    async (err, result) => {
-      if (err)
-        return res
-          .status(500)
-          .json({ message: "Erro de server. Tente novamente depois." });
-      if (
-        result.length === 0 ||
-        !(await bcrypt.compare(password, result[0].password))
-      ) {
-        return res.status(400).send("Email ou senha inválidos");
-      }
-
-      const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: "1h" });
-      res.json({ token }); // Retorna o token ao cliente
+    "INSERT INTO game (date_game, player_name, time_match, url_image, score_game) VALUES (?, ?, ?, ?, ?)",
+    [date_game, player_name, time_match, url_image, score_game],
+    (err) => {
+      if (err) throw err;
+      res.sendStatus(201); // Game registrado com sucesso
     }
   );
-});
-
-app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-  const saltRounds = 10; // Número de rounds para salting
-
-  db.query("SELECT * FROM users WHERE email = ?", [email], (err, result) => {
-    if (err) throw err;
-    if (result.length > 0) {
-      return res.status(400).send("Usuário já existe");
-    } else {
-      bcrypt.hash(password, saltRounds, (err, hash) => {
-        // alternativa: const hashedPassword = await bcrypt.hash(password, 10) // Criptografa a senha
-        if (err) throw err;
-        db.query(
-          "INSERT INTO users (email, password) VALUES (?, ?)",
-          [email, hash],
-          (err) => {
-            if (err) throw err;
-            res.sendStatus(201); // Usuário registrado com sucesso
-          }
-        );
-      });
-    }
-  });
 });
 
 app.listen(3000, () => {
